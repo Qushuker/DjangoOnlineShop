e1abf10dcfd13e8a622bfc4d029c503a57a51081
Ray <styne.gg@gmail.com>
2025-01-04 19:02:57 +0300

    Added dropdown side menu

---
+++ main/store/static/aside.js
@@ -0,0 +1,4 @@
+document.getElementById('toggleButton').addEventListener('click', function() {
+    const menu = document.getElementById('menu');
+    menu.classList.toggle('active'); // Переключаем класс для показа/скрытия меню
+});
